726ac2d7a1b50580069442ab6f00a417a35c6174
cheipstudio-cmyk <cheipstudio@gmail.com>
2026-05-29 00:01:38 +0200

    v0.10.7 fix MaterialTheme import in MainActivity

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.secondream.novagram"
         minSdk = 26
         targetSdk = 35
-        versionCode = 69
-        versionName = "0.10.6"
+        versionCode = 70
+        versionName = "0.10.7"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
