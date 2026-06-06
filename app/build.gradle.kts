33f548141f0ec4204cef20ff9ca6767c27b30c04
cheipstudio-cmyk <cheipstudio@gmail.com>
2026-06-06 10:00:20 +0200

    fix: import fillMaxHeight

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -23,8 +23,8 @@ android {
         applicationId = "com.secondream.novagram"
         minSdk = 26
         targetSdk = 35
-        versionCode = 261
-        versionName = "0.10.199"
+        versionCode = 262
+        versionName = "0.10.200"
         ndk {
             abiFilters += listOf("arm64-v8a")
         }
